35ba2fc944087d7f29934a2cdd13030f7843c0b7
sofiasales02 <sofiasaleswk@gmail.com>
2024-09-15 18:00:46 -0300

    feat: cardinfo component on breakinfo div

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import './App.css';
 import Header from './components/Header';
+import CardInfo from './components/CardInfo';
+import arqPd from "../public/img/arquivospdf.png";
+import decor from "../public/img/decoracoes.png";
 
 function App() {
 
   return (
     <>
       <Header/>
-      <div id='home_section'>
+      <section id='home_section'>
         
         <div id='text_intro' className='container fluid'>
           <h1><span>Acervo</span><span>E.V.A</span></h1>
-          <p>Transforme sua sala de aula com estilo, conheça a diversidade de belas decorações e efeites infantis produzidos com material EVA.</p>
+          <p id='description'>Transforme sua sala de aula com estilo, conheça a diversidade de belas decorações e efeites infantis produzidos com material EVA.</p>
         </div>
 
+        <div id='break_infos'>
 
-      </div>
+          <CardInfo 
+            title={"Arquivos PDF"} 
+            description={"Uma diversidade de arquivos PDF de atividades de todos os gêneros para você trabalhar em sala de aula."} 
+            img={arqPd} 
+          />
+
+          <div className='divisor'></div>
+
+          <CardInfo 
+            title={"Enfeites de sala"} 
+            description={"Decorações educativas para enfeitar sua sala de aula e trabalhar com as crianças."} 
+            img={decor} 
+          />
+
+        </div>
+
+      </section>
     </>
   )
 }
---
+++ src/components/CardInfo/index.jsx
@@ -0,0 +1,23 @@
+import './style.css';
+
+export default function CardInfo( { title, description, img } ){
+    return (
+        <>
+          <div id='card-info'>
+            <div className='card' style={{maxWidth: "500px"}}>
+              <div className='row g-0'>
+                <div className='col-md-4' style={{ display:"flex", alignItems: "center"}}>
+                  <img src={img} alt="foto do card" />
+                </div>
+                <div className='col-md-8'>
+                  <div className='card-body'>
+                    <h5 className='card-title'>{ title }</h5>
+                    <p className='card-text'>{ description }</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+    );
+}
