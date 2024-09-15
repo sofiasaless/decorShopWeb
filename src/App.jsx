80605272ed5d154a4fe7fda8e7e27969d54a9f29
sofiasales02 <sofiasaleswk@gmail.com>
2024-09-15 15:08:00 -0300

    feat: home texts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,15 @@ function App() {
   return (
     <>
       <Header/>
+      <div id='home_section'>
+        
+        <div id='text_intro' className='container fluid'>
+          <h1><span>Acervo</span><span>E.V.A</span></h1>
+          <p>Transforme sua sala de aula com estilo, conheça a diversidade de belas decorações e efeites infantis produzidos com material EVA.</p>
+        </div>
+
+
+      </div>
     </>
   )
 }
