0655193f453c28ddf6f92506015b57a73057ca05
sofiasaless <sofiasaleswk@gmail.com>
2024-09-30 14:39:25 -0300

    feat/refactor: products page

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This web page will contain JWT (Json Web Token) for postting products service.
 
 ## To do
 
-* create a new component or login button designed for tests with JWT;
-* developer the product page, with all these ones;
-* developing the script for exposing products on initial page;
+- [ ] create a new component or login button designed for tests with JWT;
+- [x] developer the product page, with all these ones;
+- [x] developing the script for exposing products on initial page;
+- [x] turn app.jsx to a home page, separing and creating the components inside it;
--- src/components/CardProduct/index.jsx
+++ src/components/CardProduct/index.jsx
@@ -3,8 +3,8 @@ import './style.css';
 export default function CardProduct( { title, price, img } ) {
     return (
         <>
-            <div class="col" style={{maxWidth: "250px"}}>
-                <div class="card h-100 rounded-1" style={{maxWidth: "250px"}}>
+            <div className="col" style={{maxWidth: "250px"}}>
+                <div class="card h-100 rounded-1  shadow-sm" style={{maxWidth: "250px"}}>
                     <img src={ img } class="card-img-top" alt="..."/>
                     <div class="card-body text-center">
                         <h5 class="card-title mb-0 text-uppercase fw-bold">{ title }</h5>
