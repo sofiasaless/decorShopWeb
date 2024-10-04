8a849ee723b3722aec0d2bcf3998a902f30729f7
sofiasaless <sofiasaleswk@gmail.com>
2024-10-04 13:42:56 -0300

    feat: update && products page

--- src/components/CardEditProduct/index.jsx
+++ src/components/CardEditProduct/index.jsx
@@ -1,6 +1,7 @@
 import './style.css';
 
-export default function CardEditProduct( { title, price, img } ) {
+export default function CardEditProduct( { title, price, img, product } ) {
+        
     return (
         <>
             <div className="col" style={{maxWidth: "250px"}}>
@@ -13,11 +14,21 @@ export default function CardEditProduct( { title, price, img } ) {
                         </p>
 
                         <div id='btn-section' className='d-flex flex-row justify-content-evenly'>
-                            <button id='bt-edit' className="btn btn-dark rounded-4 fw-bold">
+                            <button 
+                                id='bt-edit' 
+                                className="btn btn-dark rounded-4 fw-bold"
+                                data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat"
+                            >
                                 <i class="bi bi-pencil-square m-3"></i>
                                 <span className='m-2'>Editar</span>
                             </button>
-                            <button id='bt-trash' className="btn btn-danger rounded-4 fw-bold">
+                            <button id='bt-trash' className="btn btn-danger rounded-4 fw-bold"
+                                onClick={(e) => {
+                                    e.preventDefault()
+                                    console.log(product.id)
+                                    console.log(product.name)
+                                }}
+                            >
                                 <i class="bi bi-trash3"></i>
                             </button>
                         </div>
