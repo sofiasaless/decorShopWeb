2ef4a1e166ae4e0d663d2a919cf7ed7752d4c61d
sofiasaless <sofiasaleswk@gmail.com>
2024-10-21 21:35:13 -0300

    feat: modal for delete product confirmation

--- src/components/CardEditProduct/index.jsx
+++ src/components/CardEditProduct/index.jsx
@@ -3,7 +3,8 @@ import './style.css';
 
 export default function CardEditProduct( { title, price, img, product } ) {
     
-    // getting the authorites from token
+
+    // getting the authorities from token
     const authAxios = axios.create( {
         baseURL: "http://localhost:8080/decor",
         headers: {
@@ -15,20 +16,25 @@ export default function CardEditProduct( { title, price, img, product } ) {
         authAxios.delete(`/${product.id}`)
         .then((res) => {
             console.log('exclusão feita com sucesso: ' + res);
+            handleReload();
         })
         .catch((err) => {
             console.log('erro ao excluir produto: ' + err);
         })
     }
 
+    const handleReload = () => {
+        window.location.reload();
+    };
+
     return (
         <>
             <div className="col" style={{maxWidth: "250px"}}>
-                <div class="card h-100 rounded-1  shadow-sm" style={{maxWidth: "250px"}}>
-                    <img src={ img } class="card-img-top" alt="..."/>
-                    <div class="card-body text-center p-3">
-                        <h5 class="card-title mb-0 text-uppercase fw-bold">{ title }</h5>
-                        <p class="card-text">
+                <div className="card h-100 rounded-1 shadow-sm" style={{maxWidth: "250px"}}>
+                    <img src={ img } className="card-img-top" alt="..."/>
+                    <div className="card-body text-center p-3">
+                        <h5 className="card-title mb-0 text-uppercase fw-bold">{ title }</h5>
+                        <p className="card-text">
                             R${ price }
                         </p>
 
@@ -36,27 +42,62 @@ export default function CardEditProduct( { title, price, img, product } ) {
                             <button 
                                 id='bt-edit' 
                                 className="btn btn-dark rounded-4 fw-bold"
-                                data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat"
+                                data-bs-toggle="modal" 
+                                data-bs-target={`#editModal-${product.id}`}
+                                data-bs-whatever="@fat"
                             >
-                                <i class="bi bi-pencil-square m-3"></i>
+                                <i className="bi bi-pencil-square m-3"></i>
                                 <span className='m-2'>Editar</span>
                             </button>
                             <button 
+                                type="button" 
+                                data-bs-toggle="modal" 
+                                data-bs-target={`#deleteModal-${product.id}`}
                                 id='bt-trash' 
                                 className="btn btn-danger rounded-4 fw-bold" 
-
-                                onClick={(e) => {
-                                    e.preventDefault()
-                                    console.log(product.id)
-                                    console.log(product.name)
-
-                                    deleteProduct();
-                                }}
                             >
-                                <i class="bi bi-trash3"></i>
+                                <i className="bi bi-trash3"></i>
                             </button>
                         </div>
 
+                    </div>
+                </div>
+            </div>
+            
+            {/* bootstrapt modal for update product form  */}
+            <div className="modal fade" id={`editModal-${product.id}`} tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title text-uppercase fw-bold" id="editModalLabel">Editar produto</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            {product.id} - {product.name}
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                            <button type="button" id='save-btn' className="btn">Salvar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* bootstrapt modal for delete product  */}
+            <div className="modal fade" id={`deleteModal-${product.id}`} data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title text-uppercase fw-bold" id="deleteModalLabel">Confirmar Exclusão</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            Tem certeza que deseja excluir o produto <strong>"{product.name}"</strong>?
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            <button type="button" className="btn btn-danger" onClick={deleteProduct}>Excluir</button>
+                        </div>
                     </div>
                 </div>
             </div>
